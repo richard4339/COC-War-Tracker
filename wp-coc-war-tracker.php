7c93f2c48c90df32912a89570dd6d0eda4eb62d9
Richard Lynskey <richard@mozor.net>
2015-03-28 14:21:30 -0500

    Adds basic strike editing

--- wp-coc-war-tracker.php
+++ wp-coc-war-tracker.php
@@ -21,4 +21,53 @@
   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+/*
+ *
+ */
+function cwt_custom_user_profile_fields($user)
+{
+
+    if (current_user_can('promote_users')) {
+        $disabled = '';
+    } else {
+        $disabled = ' disabled="disabled"';
+    }
+    ?>
+    <table class="form-table">
+        <tr>
+            <th>
+                <label for="cwt_strikes"><?php _e('Strikes'); ?></label>
+            </th>
+            <td>
+                <input type="text" name="cwt_strikes" id="cwt_strikes"
+                       value="<?php echo esc_attr(get_the_author_meta('cwt_strikes', $user->ID)); ?>"
+                       class="regular-text" <?php echo $disabled; ?>/>
+                <br><span class="description"><?php _e("Your strikes. (3 and you're out!)", 'Your strikes.'); ?></span>
+            </td>
+        </tr>
+    </table>
+<?php
+}
+
+/**
+ * Fires before the page loads on the 'Edit User' screen.
+ *
+ * @param int $user_id The user ID.
+ */
+function cwt_update_user($user_id)
+{
+
+    if (current_user_can('promote_users')) {
+        if ($_POST['cwt_strikes'] == '') {
+            delete_usermeta($user_id, 'cwt_strikes');
+        } else {
+            update_usermeta($user_id, 'cwt_strikes', $_POST['cwt_strikes']);
+        }
+    }
+}
+
+add_action('show_user_profile', 'cwt_custom_user_profile_fields');
+add_action('edit_user_profile', 'cwt_custom_user_profile_fields');
+add_action('edit_user_profile_update', 'cwt_update_user');
+
 ?>
