7d7549525b1fef68c1e27d8bafeaa55ada03f1c0
Richard Lynskey <richard@mozor.net>
2015-03-28 12:58:27 -0500

    Initial

---
+++ wp-coc-war-tracker.php
@@ -0,0 +1,24 @@
+<?php
+/*
+  Plugin Name: Clash of Clans War Tracker
+  Plugin URI: https://github.com/richard4339/coc-war-tracker
+  Description: Plugin allows for tracking of wars for Clash of Clans
+  Version: 0.0.1
+  Author: Richard
+  Author URI: http://www.digitalxero.com
+ */
+/*  Copyright 2015  RICHARD LYNSKEY (email : richard@mozor.net)
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+?>
